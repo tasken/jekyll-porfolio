939d74d13831127cf00ac9c8c1d6e007370302da
Augusto <tasken@gmail.com>
2018-07-13 00:50:09 -0300

    rakefile refactor

--- rakefile.rb
+++ rakefile.rb
@@ -1,17 +1,34 @@
-task :default do
+task :default => [:dev]
+
+task :dev=> [:clean] do |t|
+    puts 'Serving development site'
+    sh 'JEKYLL_ENV=development bundle exec jekyll serve'
+end
+
+task :prod => [:clean] do |t|
+    puts 'Serving production site'
+    sh 'JEKYLL_ENV=production bundle exec jekyll serve'
+end
+
+task :build => [:clean] do |t|
+    puts 'Building development site'
+    sh 'JEKYLL_ENV=development bundle exec jekyll build'
+end
+
+task :build_prod => [:clean] do |t|
+    puts 'Building production site'
     sh 'JEKYLL_ENV=production bundle exec jekyll build'
 end
 
-task :serve do
-    sh 'bundle exec jekyll serve --destination _site/'
-end
-
-task :serveprod do
-    sh 'JEKYLL_ENV=production bundle exec jekyll serve --destination _site/'
-end
-
+# Maintenance tasks
 task :clean do
-    sh 'bundle exec jekyll clean --destination _site/'
+    puts 'Cleaning Jekyll files'
+    sh 'bundle exec jekyll clean'
+end
+
+task :image_optim => [:clean] do |t|
+    puts 'Crunch site images using image_optim'
+    sh 'image_optim -r . --verbose'
 end
 
 task :reset do
